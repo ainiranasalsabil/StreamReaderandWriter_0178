01a591ade97f9b349a0c39055353e314af4526a2
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-06-04 10:49:41 +0700

    menggunakan path dengan membuat variabel NamaFile

--- StreamReaderandWriter_0178/IoFile.cpp
+++ StreamReaderandWriter_0178/IoFile.cpp
@@ -30,4 +30,23 @@ int main()
 
     //membuka file dalam mede membaca
     ifstream infile;
-    
+    //menunjuk ke sebuah file 
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilakan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
