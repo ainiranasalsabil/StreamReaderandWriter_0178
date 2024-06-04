3cb906737b9ca0b2fd3b1b97a0b66e323968024e
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-06-04 10:21:31 +0700

    mendapatkan semua karakter dalam satu daris

--- StreamReaderandWriter_0178/StreamReaderandWriter_0178.cpp
+++ StreamReaderandWriter_0178/StreamReaderandWriter_0178.cpp
@@ -18,4 +18,6 @@ int main()
     //unlimited loop untuk menulis
     while (true) {
         cout << "- ";
-       
+        //mendapatkan setiap karakter dalam satu baris 
+        getline(cin, baris);
+        
