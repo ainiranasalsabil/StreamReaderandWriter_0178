b2f1c4e7a21979e17e15becc32dfdc1d2c926f44
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-06-04 10:20:37 +0700

    membuat printah untuk menulis

--- StreamReaderandWriter_0178/StreamReaderandWriter_0178.cpp
+++ StreamReaderandWriter_0178/StreamReaderandWriter_0178.cpp
@@ -13,4 +13,9 @@ int main()
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
 
-    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+       
